e087cc85170d4b3fb621428e95b395499cf2f4d4
Alina-Alen <alina.alen.2003@gmail.com>
2023-01-22 06:48:21 -0500

    Once the counter of strikes reaches 3 it loads death scene

--- Bird-Strike2d/Assets/Scripts/StrikeNum.cs
+++ Bird-Strike2d/Assets/Scripts/StrikeNum.cs
@@ -2,15 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class StrikeNum : MonoBehaviour
 {
     [SerializeField] private TMP_Text text;
     [SerializeField] private GameStats gameStats;
+    private float oneSecondwait = 1f;
 
     void Update()
     {
         int numOfStikes = 3 - gameStats.healthLeft;
         text.text = numOfStikes.ToString(); 
+
+        if(numOfStikes == 3)
+        {
+            if (oneSecondwait > 0)
+            {
+                oneSecondwait -= Time.deltaTime;
+
+            }
+            else SceneManager.LoadScene(1);
+        }
     }
 }
