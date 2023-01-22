7d2d7be02727e6568eb2616c371f9551b8b67a69
Alina-Alen <alina.alen.2003@gmail.com>
2023-01-21 23:47:49 -0500

    Bird behaviour fix (try 1)

--- Bird-Strike2d/Assets/Scripts/BirdS/BirdManager.cs
+++ Bird-Strike2d/Assets/Scripts/BirdS/BirdManager.cs
@@ -13,7 +13,7 @@ public class BirdManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        timeUntilNextBird = Random.Range(3, 5);
+        timeUntilNextBird = Random.Range(10, 15);
         
     }
 
