53c2c8d65c26c060582791932c15158498a903a9
yaro0 <yaroslava11@hotmail.com>
2023-01-22 01:57:10 -0500

    Fixed some bugs
    The camera now moves properly during turbulences

---
+++ Bird-Strike2d/Assets/Scripts/Positions.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Positions")]
+public class Positions : ScriptableObject
+{
+    public Vector3 camPos = new Vector3(0, 0, -10);
+
+
+}
